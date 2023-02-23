a87a18539ace1df3136eedceeefa4ce9c23b09f0
sunyanguo <dacaiguoguo@163.com>
2023-02-23 17:52:57 +0800

    优化UI

--- MergeVideo/ViewController.m
+++ MergeVideo/ViewController.m
@@ -11,15 +11,64 @@
 @import MobileCoreServices;
 @import UniformTypeIdentifiers;
 
-@interface ViewController ()<PHPickerViewControllerDelegate>
+@interface ViewController ()<PHPickerViewControllerDelegate, UITableViewDelegate, UITableViewDataSource>
 @property (nonatomic, strong) NSMutableArray *mp4Array;
+@property (nonatomic, strong) UITableView *tableView;
 @end
 
 @implementation ViewController
+#pragma mark tableView lazy
+-(UITableView *)tableView {
+    if (!_tableView) {
+        _tableView = [[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+    }
+    return _tableView;
+}
 
+#pragma tableView UITableViewDataSource
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.mp4Array.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *identifier = @"identifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identifier];
+        UIProgressView *pview = [[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleDefault];
+        [cell.contentView addSubview:pview];
+        pview.tag = 102;
+        pview.frame = CGRectMake(0, 0, self.view.bounds.size.width, 10);
+    }
+    NSURL *obj = self.mp4Array[indexPath.row];
+    cell.textLabel.text = obj.description;
+    UIProgressView *pview = [cell.contentView viewWithTag:102];
+    if ([obj isKindOfClass:NSProgress.class]){
+        pview.observedProgress = (NSProgress *)obj;
+    }
+    if ([obj isKindOfClass:NSURL.class]){
+        pview.observedProgress = nil;
+        [pview setProgress:0.0 animated:NO];
+        cell.textLabel.text = obj.path.lastPathComponent;
+    }
+    return cell;
+}
+
+#pragma tableView--UITableViewDelegate
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.mp4Array = [NSMutableArray array];
+    [self.view addSubview:self.tableView];
+    _tableView.frame = self.view.bounds;
     NSLog(@"%@", NSHomeDirectory());
     if (@available(iOS 14, *)) {
         PHPickerConfiguration *config = [[PHPickerConfiguration alloc] init];
@@ -41,81 +90,60 @@
         [results enumerateObjectsUsingBlock:^(PHPickerResult * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
             [self.mp4Array addObject:[NSNull null]];
         }];
-
+        [self.tableView reloadData];
         [results enumerateObjectsUsingBlock:^(PHPickerResult * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
             // Your program should copy or move the file within the completion handler.
             // 需要在block内copy或者move文件 因为block执行后文件会被删除
-            [obj.itemProvider loadFileRepresentationForTypeIdentifier:UTTypeMovie.identifier completionHandler:^(NSURL * _Nullable item, NSError * _Nullable error) {
-
+            NSProgress *logp = [obj.itemProvider loadFileRepresentationForTypeIdentifier:UTTypeMovie.identifier
+                                                                       completionHandler:^(NSURL * _Nullable item, NSError * _Nullable error) {
                 NSFileManager *fileManager = [NSFileManager defaultManager];
                 NSError *err = nil;
                 NSURL *docUrl = [fileManager URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:YES error:&err];
                 NSURL *wurl = [docUrl URLByAppendingPathComponent:[NSString stringWithFormat:@"%@.mp4", @(idx)]];
                 [fileManager removeItemAtURL:wurl error:&err];
                 [fileManager copyItemAtURL:item toURL:wurl error:&err];
-
-                [self.mp4Array replaceObjectAtIndex:idx withObject:wurl];
-                BOOL hasNull = NO;
-                // 检查是否存在 null
-                for (id item in self.mp4Array) {
-                    if([item isKindOfClass:NSNull.class]) {
-                        hasNull = YES;
+                NSLog(@"等待合并%@", NSThread.currentThread);
+                // completionHandler 在不同的 线程，是否考虑线程安全问题？
+                dispatch_sync(dispatch_get_main_queue(), ^{
+                    // 总是在主线程操作可变数组，保证结果正确
+                    [self.mp4Array replaceObjectAtIndex:idx withObject:wurl];
+                    [self.tableView reloadData];
+                    BOOL hasNull = NO;
+                    // 检查是否存在 null
+                    for (id item in self.mp4Array) {
+                        if(![item isKindOfClass:NSURL.class]) {
+                            hasNull = YES;
+                        }
                     }
-                }
 
-                NSLog(@"等待合并");
-                if (!hasNull) {
-                    // 都取到了，就合并
-                    NSLog(@"开始合并");
-                    [self mergeVideoToOneVideo:self.mp4Array toStorePath:@"join" WithStoreName:@"合并后的视频" success:^(NSURL *fileurl){
-                        //如果想分享图片 就把图片添加进去 文字什么的同上
-                        NSArray *activityItems = @[fileurl];
-                        // 创建分享vc
-                        UIActivityViewController *activityVC = [[UIActivityViewController alloc]initWithActivityItems:activityItems applicationActivities:nil];
-                        // 设置不出现在活动的项目
-                        activityVC.excludedActivityTypes =
-                        @[UIActivityTypePrint,UIActivityTypeMessage,UIActivityTypeMail,
-                          UIActivityTypePrint,UIActivityTypeAddToReadingList,UIActivityTypeOpenInIBooks,
-                          UIActivityTypeCopyToPasteboard,UIActivityTypeAssignToContact];
+                    if (!hasNull) {
+                        [self mergeAndShare];
+                    }
+                });
 
-                        [self presentViewController:activityVC animated:YES completion:nil];
-                        // 分享之后的回调
-                        activityVC.completionWithItemsHandler = ^(UIActivityType  _Nullable activityType, BOOL completed, NSArray * _Nullable returnedItems, NSError * _Nullable activityError) {
-                            if (completed) {
-                                //分享 成功
-                            } else  {
-                                //分享 取消
-                            }
-                        };
-
-
-                    } failure:^{
-
-                    }];
-
-
-                }
             }];
-            //            [obj.itemProvider loadItemForTypeIdentifier:UTTypeMovie.identifier options:nil completionHandler:^(NSURL *item, NSError * _Null_unspecified error) {
-            //
-            //            }];
+            NSLog(@"等待logp%@", logp);
+            [self.mp4Array replaceObjectAtIndex:idx withObject:logp];
+            [self.tableView reloadData];
         }];
     }];
 }
 
-/**
- *  多个视频合成为一个视频输出到指定路径,注意区分是否3D视频
- *
- *  @param tArray       视频文件NSURL地址
- *  @param storePath    沙盒目录下的文件夹
- *  @param storeName    合成的文件名字
- *  @param successBlock 成功block
- *  @param failureBlcok 失败block
- */
--(void)mergeVideoToOneVideo:(NSArray *)tArray toStorePath:(NSString *)storePath WithStoreName:(NSString *)storeName success:(void (^)(NSURL *fileurl))successBlock failure:(void (^)(void))failureBlcok
-{
-    AVMutableComposition *mixComposition = [self mergeVideostoOneVideo:tArray];
-    NSURL *outputFileUrl = [self joinStorePath:storePath togetherStoreName:storeName];
+- (void)mergeAndShare {
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSError *err = nil;
+    NSURL *docUrl = [fileManager URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:YES error:&err];
+    // 都取到了，就合并
+    NSLog(@"开始合并");
+    NSURL *destDirectory = [docUrl URLByAppendingPathComponent:@"join"];
+    if ([fileManager fileExistsAtPath:destDirectory.path isDirectory:nil]) {
+        [fileManager removeItemAtURL:destDirectory error:&err];
+    }
+    [fileManager createDirectoryAtURL:destDirectory withIntermediateDirectories:YES attributes:nil error:&err];
+    NSURL *outputFileUrl = [[docUrl URLByAppendingPathComponent:@"join"] URLByAppendingPathComponent:@"合并后的视频.mp4"];
+
+
+    AVMutableComposition *mixComposition = [self mergeVideostoOneVideo:self.mp4Array];
     AVAssetExportSession* _assetExport = [AVAssetExportSession exportSessionWithAsset:mixComposition presetName:AVAssetExportPresetHighestQuality];
     NSLog(@"%@", [_assetExport supportedFileTypes]);
     /*
@@ -133,8 +161,25 @@
         NSLog(@"exportAsynchronouslyWithCompletionHandler: %li\n", (long)status);
         if (status == AVAssetExportSessionStatusCompleted) {
             dispatch_async(dispatch_get_main_queue(), ^{
-                //在系统相册存储一份
-                successBlock(outputFileUrl);
+                //如果想分享图片 就把图片添加进去 文字什么的同上
+                NSArray *activityItems = @[outputFileUrl];
+                // 创建分享vc
+                UIActivityViewController *activityVC = [[UIActivityViewController alloc]initWithActivityItems:activityItems applicationActivities:nil];
+                // 设置不出现在活动的项目
+                activityVC.excludedActivityTypes =
+                @[UIActivityTypePrint,UIActivityTypeMessage,UIActivityTypeMail,
+                  UIActivityTypePrint,UIActivityTypeAddToReadingList,UIActivityTypeOpenInIBooks,
+                  UIActivityTypeCopyToPasteboard,UIActivityTypeAssignToContact];
+
+                [self presentViewController:activityVC animated:YES completion:nil];
+                // 分享之后的回调
+                activityVC.completionWithItemsHandler = ^(UIActivityType  _Nullable activityType, BOOL completed, NSArray * _Nullable returnedItems, NSError * _Nullable activityError) {
+                    if (completed) {
+                        //分享 成功
+                    } else  {
+                        //分享 取消
+                    }
+                };
             });
         }
     }];
@@ -179,30 +224,6 @@
     }];
 
     return mixComposition;
-}
-/**
- *  拼接url地址
- *
- *  @param sPath 沙盒文件夹名
- *  @param sName 文件名称
- *
- *  @return 返回拼接好的url地址
- */
--(NSURL *)joinStorePath:(NSString *)sPath togetherStoreName:(NSString *)sName {
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *documentPath = [paths objectAtIndex:0];
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-
-    NSString *storePath = [documentPath stringByAppendingPathComponent:sPath];
-    BOOL isExist = [fileManager fileExistsAtPath:storePath];
-    if (isExist) {
-        [fileManager removeItemAtPath:storePath error:nil];
-    }
-    [fileManager createDirectoryAtPath:storePath withIntermediateDirectories:YES attributes:nil error:nil];
-    NSString *realName = [NSString stringWithFormat:@"%@.mp4", sName];
-    storePath = [storePath stringByAppendingPathComponent:realName];
-    NSURL *outputFileUrl = [NSURL fileURLWithPath:storePath];
-    return outputFileUrl;
 }
 
 
